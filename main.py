007a71491bce4a5886f193f8420bb7dff1131187
Felipe França <59803453+DinnoFe@users.noreply.github.com>
2024-08-08 00:05:36 -0300

    Funcao e  Loop - Gerar Mob

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from random import randint
 
-lista_npcs = []
-
+lista_mobs = []
 
 def creat_monster():
     level =randint(0, 50)
 
-    novo_npc = {
+    new_mob = {
         "nome": f"Monstro #{level}#",
         "level": level,
         "dano": 5 * level,
         "hp": 100 * level,
     }
 
-    lista_npcs.append(novo_npc)
+    return new_mob
 
+def gerar_mobs(n_mobs):  
+    for x in range(n_mobs):
+        new_mob = creat_monster()
+        lista_mobs.append(new_mob)
+def exibir_mobs():
+    for mob in lista_mobs:
+        print(
+            f"Nome: {mob['nome']} // Level: {mob['level']} // Dano: {mob['dano']} // HP: {mob['hp']}"
+        )
 
-creat_monster()
+gerar_mobs(10)
+exibir_mobs()
 
-print(lista_npcs)
